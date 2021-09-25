43fa0c30b1fb16dcf2c811939443d1c2b10d3b13
Ope Abidemi Harry <opeabidemi@gmail.com>
2021-09-25 14:04:13 +0000

    added static files

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json())
+
+app.use(express.static("public"))
+
+app.listen(3000, () => {
+    console.log("Server has started 🚀🚀");
+})
